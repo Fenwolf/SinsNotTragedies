290332eb8be3b179b036023c503663c169968e43
Fén <darkone238@gmail.com>
2023-03-13 01:04:27 -0700

    Move comment block

--- Patches/Sticky Sprites/stickyspriteslevel.asm
+++ Patches/Sticky Sprites/stickyspriteslevel.asm
@@ -1,3 +1,5 @@
+; Simple example UberASM to enable Sticky Sprites that could be for a level or GM14
+
 ; If you changed !FreeRAM in StickySprites.asm, make sure to change this too
 !FreeRAM = $1864
 
@@ -7,7 +9,6 @@ else
 	!FreeRAMRemapped = !FreeRAM
 endif
 
-; Simple example UberASM to enable Sticky Sprites that could be for a level or GM14
 init:
 	lda #$01
 	sta !FreeRAMRemapped
